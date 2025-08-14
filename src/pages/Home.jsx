571349db3fb52cc8944b15b9017ec53808c3267c
Atokolo John <atokoloj@gmail.com>
2025-08-14 21:37:17 +0100

    updated UI: worked on the content of the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <div>
-      <h2>Home Page</h2>
-    </div>
-  )
+const Home =()=> {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+    
+
+    return (
+        <main>
+            <form action={addIngredient} className="add-ingredient-form">
+                <h1>Find Your Perfect Recipe</h1>
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
+        </main>
+    )
 }
 
 export default Home
